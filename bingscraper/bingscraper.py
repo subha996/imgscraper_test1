f836361da60a1c68360f05d8e2939cedbc6a6b46
Subhabrata Nath <85347886+subha996@users.noreply.github.com>
2021-08-31 23:46:54 +0530

    Update bingscraper.py

--- bingscraper/bingscraper.py
+++ bingscraper/bingscraper.py
@@ -31,14 +31,18 @@ class BingScraper():
                 os.makedirs(self.download_folder) # creating download folder
             
             chrome_options = webdriver.ChromeOptions()
-            # chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--headless')
             # self.chrome_options.headless = True
             # self.chrome_options.add_argument('--disable-gpu')
-            chrome_options.add_argument('--disable-software-rasterizer')
+#             chrome_options.add_argument("--disable-dev-shm-usage")
+#             chrome_options.add_argument('--disable-software-rasterizer')
             # self.chrome_options.add_argument('-enable-webgl')
-            chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
-            self.browser = webdriver.Chrome(executable_path=self.driver_path, options=chrome_options)
+            chrome_options.add_argument('--no-sandbox')
+#             chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+#           self.browser = webdriver.Chrome(executable_path=self.driver_path, options=chrome_options)
+            self.browser = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
             self.log.info("Bing Scrapper Initialized")
             self.filepath = "urls\\" + str(self.search_query) + ".txt"
 
